f14ddc6da75f5eb148e383b9fdf933c475967655
Punnapareddy Bhaskar Rao <bhasaprp2020@gmail.com>
2025-06-14 16:09:47 +0530

    Completed project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI project',
+    version='0.0.0',
+    author='Punnapareddy Bhaskar Rao',
+    author_email='bhasaprp2020@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
